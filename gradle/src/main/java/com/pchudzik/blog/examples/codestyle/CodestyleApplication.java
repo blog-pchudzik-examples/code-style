13ed22d2eef3914d40c70c170b7733c72bb2336f
Paweł Chudzik <pawel.chudzik@gmail.com>
2019-06-26 18:31:26 +0200

    initial commit

---
+++ gradle/src/main/java/com/pchudzik/blog/examples/codestyle/CodestyleApplication.java
@@ -0,0 +1,11 @@
+package com.pchudzik.blog.examples.codestyle;
+
+public class CodestyleApplication
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world");
+    }
+
+}
---
+++ maven/src/main/java/com/pchudzik/blog/examples/codestyle/App.java
@@ -0,0 +1,13 @@
+package com.pchudzik.blog.examples.codestyle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
